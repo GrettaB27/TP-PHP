351de331ca367cf86bb5089049e5b3e3c2f73835
grettabula <grettabula@outlook.com>
2022-02-09 10:10:57 +0100

    first commit

---
+++ utilisateur.php
@@ -0,0 +1,24 @@
+<?php
+//**********tableau utilisateur******* */
+$entete1 = ["nom","prenom","email","age"];
+$utilisateur =  [
+    [
+        "nom" => "BULA ",
+        "prenom" => "Gretta",
+        "email" => "Gb@outlook.com",
+        "age"  =>  28 
+    ],
+    [
+        "nom" => "K",
+        "prenom" => "C",
+        "email" => "KC@outlook.com",
+        "age"  =>  32
+    ],
+    [
+        "nom" => "K",
+        "prenom" => "T",
+        "email" => "KT@outlook.com",
+        "age"  =>  1
+    ],
+];
+?>
